7e12ef80d0a114773568182b2e23b3d196e929a1
Boii <bayukristanto2005@gmail.com>
2025-09-25 09:18:49 +0700

    commit aktivitas komunitas

--- resources/views/components/aktifkom.blade.php
+++ resources/views/components/aktifkom.blade.php
@@ -1,3 +1,30 @@
-<section id="Aktivitas Komunitas">
-    <h1>hai</h1>
+<section id="aktivitas-komunitas">
+    <div class="aktivitas-komunitas-content">
+        <div class="title-aktivitas-komunitas">
+            <h1>Aktivitas Komunitas</h1>
+            <p>Berbagai kegiatan menarik untuk memperkuat ikatan budaya</p>
+        </div>
+        <div class="body-aktivitas-komunitas">
+            <div class="box box-left">
+                <i class="bi bi-book"></i>
+                <h4>Workshop</h4>
+                <p>12 Event Bulan Ini</p>
+            </div>
+            <div class="box box-center">
+                <i class="bi bi-person-plus-fill"></i>
+                <h4>Diskusi</h4>
+                <p>45 Topik Aktif</p>
+            </div>
+            <div class="box box-right">
+                <i class="bi bi-trophy"></i>
+                <h4>Kompetisi</h4>
+                <p>5 Lomba Berlangsung</p>
+            </div>
+            <div class="box box-right">
+                <i class="bi bi-trophy"></i>
+                <h4>Webinar</h4>
+                <p>8 Sesi Mendatang</p>
+            </div>
+        </div>
+    </div>
 </section>
